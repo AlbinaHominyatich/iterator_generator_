29e8279ae44c63d2c0b0f1406cc432006a377730
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-18 20:42:22 +0300

    перше завдання

--- main.py
+++ main.py
@@ -1,42 +1,16 @@
-#ітератор
-lst = [1,2,3,4,5,6,6]
-print(iter(lst))
+#2
+class SquareGenerator:
+    def __init__(self, n):
+       if not isinstance(n, (int, float)):
+           raise TypeError("це повинно бути числом")
+       self.n = n
+    def generator_square(self):
+        for i in range(1, int(self.n) + 1):
+            yield  i ** 2
+#приклад використання
 
-class MyIterator:
-    def __init__(self, data):
-        self.data = data
-        self.index = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.index >= len(self.data):
-            raise StopIteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-for num in MyIterator(lst):
-    print(num)
-
-def my_generator(data):
-    for item in data:
-        yield item
-
-for num in my_generator(lst):
-    print(num)
-
-def calc():
-    def add(a, b):
-        return a + b
-    def sub (a, b):
-        return a - b
-    def mult (a, b):
-        return a * b
-    def div(a, b):
-        if b != 0:
-            return a / b
-        else:
-            raise ValueError("Помилка! Ділення на нуль!")
-    return  add, sub, mult, div
-add, sub, mult, div = calc() #відбувається замикання
-print(div(3, 1))
-
+try:
+    for num in SquareGenerator(20).generator_square():
+        print(num)
+except TypeError as e:
+    print (e)
